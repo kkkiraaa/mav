175245576b169a3b4d3df70de90045d5bb162dfb
Yroslav <153438461+Yroslav18@users.noreply.github.com>
2025-03-15 16:31:31 +0500

    ImageSearch.vue

--- src/components/ImageSearch.vue
+++ src/components/ImageSearch.vue
@@ -55,28 +55,79 @@ export default {
 
 <style scoped>
 .search {
-  color: #e0e0e0;
-  background-color:rgb(246, 223, 178, 0.77);
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 0 10px #9b59b6;
-}
-input {
-  color: #e0e0e0;
-  background-color: #4a004a;
+
+  display: inline-block;
+  padding: 12px 24px;
+  width: 450px;
+  height: 140px;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+
+  background-color: #6ecdf586;
+  color: #ffffff;
   border: none;
-  padding: 10px;
-  border-radius: 5px;
+  border-radius: 8px;
+  text-shadow: 0 0 20px #9b59b6;
+  transition: background-color 0.3s ease;
 }
+
+.search:hover {
+  background-color: #2490f5bb;
+}
+a.searchn {
+  display: inline-block;
+}
+.input {
+  display: inline-block;
+  padding: 12px 24px;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  background-color: #6ecdf586;
+  color: #ffffff;
+  border: none;
+  border-radius: 8px;
+
+  transition: background-color 0.3s ease;
+}
+.input:hover {
+  background-color: #2490f5bb;
+}
+.input:active {
+  background-color: #0b6db9;
+  transform: translateY(2px);
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); 
+}
+a.input {
+  display: inline-block;}
 button {
+  display: inline-block;
+  padding: 12px 24px;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  border: none;
+  border-radius: 8px;
+  transition: background-color 0.3s ease;
   background-color: #8e44ad;
   color: white;
   border: none;
-  padding: 10px 20px;
   cursor: pointer;
   transition: background-color 0.3s;
 }
 button:hover {
-  background-color: #732d91;
+  background-color: #a449efc0;
 }
+button:active {
+  background-color: #7114bdc0;
+  transform: translateY(2px);
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); 
+}
+
+
 </style>
