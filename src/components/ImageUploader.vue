29a7a902dd0ad2a176a9d023267f970e21ee7791
Yroslav <153438461+Yroslav18@users.noreply.github.com>
2025-03-15 16:32:19 +0500

    ImageUploader.vue

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -35,24 +35,52 @@ export default {
 
 <style scoped>
 .uploader {
-  color: #e0e0e0;
-  background-color: #3a003c;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 0 10px #9b59b6;
+
+display: inline-block;
+padding: 12px 24px;
+width: 450px;
+height: 140px;
+text-align: center;
+text-decoration: none;
+cursor: pointer;
+
+background-color: #6ecdf586;
+color: #ffffff;
+border: none;
+border-radius: 8px;
+
+transition: background-color 0.3s ease;
+}
+
+.uploader:hover {
+background-color: #2490f5bb;
 }
 input[type="file"] {
   color: #e0e0e0;
 }
 button {
+  display: inline-block;
+  padding: 12px 24px;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  border: none;
+  border-radius: 8px;
+  transition: background-color 0.3s ease;
   background-color: #8e44ad;
   color: white;
   border: none;
-  padding: 10px 20px;
   cursor: pointer;
   transition: background-color 0.3s;
 }
 button:hover {
-  background-color: #732d91;
+  background-color: #a449efc0;
+}
+button:active {
+  background-color: #7114bdc0;
+  transform: translateY(2px);
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); 
 }
 </style>
